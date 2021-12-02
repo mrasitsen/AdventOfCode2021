2c5431b7f9705cde1ecccc54c061ca38d9e27f28
mrsen <muhammetrasitsen@gmail.com>
2021-12-02 17:40:31 +0100

    Day 2 part 1

---
+++ src/Day2_2.java
@@ -0,0 +1,28 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Day2_2 {
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        int totalX = 0, totalY = 0 , magnitute, aim = 0;
+        String command;
+
+        Scanner sc = new Scanner(new FileReader("file/day2.txt"));
+        while(sc.hasNext()){
+            command = sc.next();
+            magnitute = sc.nextInt();
+
+            switch (command) {
+                case "forward" -> {
+                    totalX += magnitute;
+                    totalY += (aim * magnitute);
+                }
+                case "up" -> aim -= magnitute;
+                case "down" -> aim += magnitute;
+            }
+        }
+        System.out.println(totalX * totalY);
+    }
+}
